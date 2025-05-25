2a3c15db570f7bce9a254dc702e0fc879b8ff827
KevinPCanada <kevinpcanada@gmail.com>
2025-05-25 21:51:37 +0900

    Installed shadcnui and did further tailwind config

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Footer.css'
 import Logo from './../../assets/img/placeholder-logo-1.png'
+import { Button } from "../ui/button"
 
 const Footer = () => {
   return (
     <footer>
+      <Button>Click me</Button>
       <img src={Logo} alt="" className='footerLogo'/>
       <span>This website is demo and not intended for commercial use.</span>
     </footer>
