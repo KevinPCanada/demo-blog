2d08d53d89110d5a98d192bfdab9f3d0402ec218
KevinPCanada <kevinpcanada@gmail.com>
2025-12-29 00:46:51 +0900

    db.js updated to fix deployment bug

--- api/db.js
+++ api/db.js
@@ -8,13 +8,14 @@ export const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 4000,
   ssl: {
     minVersion: "TLSv1.2",
     rejectUnauthorized: true,
   },
   // Pool specific settings
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 5,
   queueLimit: 0,
+  connectTimeout: 60000, // Give TiDB 60 seconds to wake up 
 });
